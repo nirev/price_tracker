871e2eba1244c28899dc37c138295fcc3bc4e823
nirev <guilherme@nirev.org>
2017-10-13 18:36:17 -0300

    Add credo, dialyzer, docs

--- mix.exs
+++ mix.exs
@@ -1,15 +1,25 @@
 defmodule PriceTracker.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
   def project do
     [
       app: :price_tracker,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_options: [warnings_as_errors: true],
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      dialyzer: [ flags: [:error_handling, :race_conditions],
+                  remove_defaults: [:unknown]],
+
+      # Docs
+      name: "Price Tracker Test",
+      docs: [output: "./docs",
+             canonical: "https://nirev.github.io/price_tracker",
+             source_url: "https://github.com/nirev/price_tracker"]
     ]
   end
 
@@ -18,7 +28,8 @@ defmodule PriceTracker.Mixfile do
 
   defp aliases do
     [
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+      "analysis": ["credo --strict", "dialyzer"]
     ]
   end
 
@@ -33,10 +44,14 @@ defmodule PriceTracker.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:postgrex, ">= 0.0.0"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:earmark, "~> 1.1", only: :dev},
       {:ecto, "~> 2.1"},
+      {:ex_doc, "~> 0.16", only: :dev},
+      {:mox, "~> 0.2.0", only: :test},
       {:poison, "~> 3.1"},
-      {:mox, "~> 0.1.0", only: :test},
+      {:postgrex, ">= 0.0.0"},
     ]
   end
 end
